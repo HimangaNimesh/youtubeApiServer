8d5b0a2f9ee1bb58015f32357358a81f65401687
himanganimesh2 <himanganimesh2@gmail.com>
2023-06-25 11:56:56 +0530

    first commit

---
+++ server/User/User.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const UserSchema = mongoose.Schema({
+    name: {
+        type:String,
+        required:true,
+        uniqe:true
+    },
+    email: {
+        type:String,
+        required:true,
+        uniqe:true
+    },
+    password: {
+        type:String,
+        required:true
+    },
+    img: {
+        type:String
+    },
+    subscribers: {
+        type:Number,
+        default:0
+    },
+    subscribedUsers: {
+        type:Array
+    },
+})
+
+export default mongoose.model("User", UserSchema)
